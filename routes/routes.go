75ffb3c16f201a86ea1432f8fd9a9e5ae65dc8df
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2024-12-06 05:50:22 +0700

    feat:auth (check email)

--- routes/routes.go
+++ routes/routes.go
@@ -13,7 +13,7 @@ func NewRoutes(ctx infra.ServiceContext) *gin.Engine {
 
 	r.POST("/login", ctx.Ctl.AuthHandler.Login)
 	r.POST("/register", ctx.Ctl.UserHandler.Registration)
-	r.GET("/users", ctx.Ctl.UserHandler.Get)
+	r.GET("/users", ctx.Ctl.UserHandler.All)
 	r.POST("/password-reset", ctx.Ctl.PasswordResetHandler.Create)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
